a7494cd3d72e43db7e0e76427a659758b62cba61
Muhammad Abdullah <133787552+abdullah0325@users.noreply.github.com>
2025-06-26 16:37:17 +0500

    Update openai_service.py

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from app.core.config import settings
 
 llm = ChatOpenAI(
-    model=gpt-4o-mini,
+    model="gpt-4o-mini",
     openai_api_key=settings.OPENAI_API_KEY,
     temperature=0.2
 )
